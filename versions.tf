42aa66d56547fad1f5d1a87a013551c762375f4f
Benjamin Sjølander <17160920+putetrekk@users.noreply.github.com>
2022-06-16 14:25:54 +0200

    Replace deprecated "proxy provider configuration"

--- versions.tf
+++ versions.tf
@@ -2,6 +2,12 @@ terraform {
   required_version = ">= 0.12"
 
   required_providers {
-    aws = ">= 2.65.0"
+    aws = {
+      source = "hashicorp/aws"
+      configuration_aliases = [
+        aws.certificate_provider
+      ]
+      version = ">= 2.65.0"
+    }
   }
 }
